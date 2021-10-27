f89c54b5abe89d01e15fbf8c336eb1fdd683111b
kristof22122 <kristf44@gmail.com>
2021-10-27 12:42:20 +0300

    add amountOfCheck function

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -34,3 +34,38 @@ const arr = [5, 7, 10, 1, 1, 11, 11, 11];
 
 console.log(findAverage1(arr));
 console.log(findAverage2(arr));
+
+// calculate the amount of the “check” (the function argument is an array of objects [{name: ‘Product”, amount: 3, price: 100}, ...], the function should return the total amount as a number)
+
+function amountOfCheck (check) {
+  let sum = 0;
+  
+  if (check.length === 0) return sum;
+
+  check.forEach(element => {
+    sum += Math.round(element.amount * element.price);
+  });
+
+  // return sum.toFixed(2);
+  return sum;
+};
+
+const check = [
+  {
+    name: 'milk',
+    amount: 3,
+    price: 32, 
+  },
+  {
+    name: 'bread',
+    amount: 2,
+    price: 20, 
+  },
+  {
+    name: 'meat',
+    amount: 1,
+    price: 180, 
+  },
+];
+
+console.log(amountOfCheck(check));
