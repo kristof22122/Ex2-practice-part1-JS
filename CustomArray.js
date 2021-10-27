329b3318d9fe920edfbeb21e2dcec9e80c4608bd
kristof22122 <kristf44@gmail.com>
2021-10-27 12:58:50 +0300

    push and pop methods added

--- CustomArray.js
+++ CustomArray.js
@@ -1,7 +1,44 @@
 'use strict';
 
 class CustomArray {
-    constructor(...arr) {
-      this.arr = arr;
+  constructor(...arr) {
+    this.arr = arr;
+  }
+
+  push(...item) {
+    const len = item.length;
+    const numbersLen = this.arr.length;
+    let index = 0;
+
+    for (let i = numbersLen; i < numbersLen + len; i++) {
+      this.arr[i] = item[index];
+      index++;
     }
+
+    return this.length;
+  }
+
+  pop() {
+    if (this.arr.length === 0) {
+      return undefined;
+    }
+
+    const lastItem = this.arr[this.arr.length - 1];
+
+    this.arr.length = this.arr.length - 1;
+
+    return lastItem;
+  }
 };
+
+const a = new CustomArray(1, 2, 3);
+const b = new CustomArray(1, 2, 3, 4);
+
+console.log(a.arr);
+console.log(b.arr);
+
+a.push(3, 4, 5);
+console.log(a.arr);
+
+console.log(b.pop());
+console.log(b.arr);
