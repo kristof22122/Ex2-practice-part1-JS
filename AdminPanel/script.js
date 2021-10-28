d4d2cfca8418aa961df03e7ec9a9375d7c14861d
kristof22122 <kristf44@gmail.com>
2021-10-28 16:59:48 +0300

    AdminPanel fixed

--- AdminPanel/script.js
+++ AdminPanel/script.js
@@ -4,26 +4,38 @@ const root = document.querySelector('.adminPanel');
 const userNameField = root.querySelector('.adminPanel__userName');
 const form = root.querySelector('.adminPanel__form');
 const table = root.querySelector('.adminPanel__table');
-const localStorageLength = localStorage.length;
+let userUpdateId = null; 
 
-let currentUsers = [
-  
+// localStorage.clear();
+
+let localStorageUsers = [
+
 ];
 
-for (let i = 0; i < localStorageLength; i++) {
-  const key = localStorage.key(i);
-  const value = localStorage.getItem(`${key}`);
-  const userValue = value.split('#');
-  currentUsers.push({
-    id: +key,
-    userName: userValue[0],
-    userDepartment: userValue[1],
-    DateOfCreation: userValue[2],
-    DateOfChange: userValue[3],
-  });
+const setLocalStorageUsers = (newLocalStorageUsers) => {
+  localStorage.setItem('localStorageUsers', JSON.stringify(newLocalStorageUsers));
+}
+
+const getLocalStorageUsers = () => {
+  const retrievedObject = localStorage.getItem('localStorageUsers');
+  
+  return JSON.parse(retrievedObject);
 };
 
-initUsers(currentUsers);
+let UsersFromLocalStorage = getLocalStorageUsers();
+
+if (UsersFromLocalStorage) {
+  localStorageUsers = [...UsersFromLocalStorage.map(user => user)];
+};
+
+initUsers(UsersFromLocalStorage);
+
+const timeFormat = (time) => {
+  if (time < 10) {
+    return `0${time}`;
+  } 
+  return `${time}`;
+};
 
 const dateBuilder = (currentDate) => {
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -32,23 +44,31 @@ const dateBuilder = (currentDate) => {
   const day = days[currentDate.getDay()];
   const date = currentDate.getDate();
   const month = months[currentDate.getMonth()];
-  const hours = currentDate.getHours();
-  const min = currentDate.getMinutes();
   const year = currentDate.getFullYear();
-  let minutes = '';
-
-  if (min < 10) {
-    minutes = `0${min}`;
-  } else {
-    minutes = `${min}`;
-  }
+  const hr = currentDate.getHours();
+  const min = currentDate.getMinutes();
+  const hours = timeFormat(hr);
+  const minutes = timeFormat(min);
 
   return `${day}, ${date} ${month} ${year}, ${hours}:${minutes}`;
 };
 
-function initUsers(users) {
-  for (const user of users) {
-    table.insertAdjacentHTML('beforeend', `
+function initUsers(Users) {
+  const tableBody = table.querySelector('.adminPanel__tableBody');
+
+  if (tableBody) {
+    tableBody.remove();
+  }  
+
+  const newTableBody = document.createElement('tbody');
+  newTableBody.className = 'adminPanel__tableBody';
+
+  table.append(newTableBody);
+
+  if (!Users) return;
+
+  for (const user of Users) {
+    newTableBody.insertAdjacentHTML('beforeend', `
       <tr
         class="adminPanel__table-item"
         data-user-id="${user.id}"
@@ -59,11 +79,6 @@ function initUsers(users) {
           >
             ${user.userName}
           </label>
-          <input
-            type="text"
-            class="adminPanel__changeUserName hidden"
-            value="${user.userName}"
-          >
         </td>
         <td>
           <label 
@@ -71,27 +86,6 @@ function initUsers(users) {
           >
             ${user.userDepartment}
           </label>
-          <select
-            name="userDepartment"
-            class="adminPanel__changeUserDepartment hidden"
-            id="userDepartment"
-          >
-            <option 
-              value="development"
-            >
-              development
-            </option>
-            <option 
-              value="bookkeeping"
-            >
-              bookkeeping
-            </option>
-            <option 
-              value="management"
-            >
-              management
-            </option>
-          </select>
         </td>
         <td>
           <label 
@@ -111,20 +105,16 @@ function initUsers(users) {
           <button
             type="button"
             class="update"
+            id="updateItem"
           >
             Update
-          </button>
-          <button
-            type="button"
-            class="save hidden"
-          >
-            Save
           </button>
         </td>
         <td>
           <button
             type="button"
             class="destroy"
+            id="deleteItem"
           >
             delete
           </button>
@@ -137,127 +127,59 @@ function initUsers(users) {
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const data = new FormData(form);
+  const dateAdd = dateBuilder(new Date());
   const id = +new Date();
-  console.log(id);
   form.elements.userName.value = '';
   form.elements.userDepartment.value = '';
 
-  currentUsers.push({
-    id: id,
-    userName: data.get('userName'),
-    userDepartment: data.get('userDepartment'),
-    DateOfCreation: dateBuilder(new Date()),
-    DateOfChange: dateBuilder(new Date()),
-  });
+  if (userUpdateId) {
+    localStorageUsers = [...localStorageUsers.map(user => {
+      const {
+        id,
+        DateOfCreation,
+      } = user;
 
-  localStorage.setItem(`${id}`,
-    `${data.get('userName')}#${data.get('userDepartment')}#${dateBuilder(new Date())}#${dateBuilder(new Date())}`);
+      if (user.id === userUpdateId) {
+        user = {
+          id: id,
+          userName: data.get('userName'),
+          userDepartment: data.get('userDepartment'),
+          DateOfCreation: DateOfCreation,
+          DateOfChange: dateAdd,
+        }
+        return user;
+      }
 
-  console.log(currentUsers);
+      return user;
+    })];
 
-  table.insertAdjacentHTML('beforeend', `
-  <tr
-    class="adminPanel__table-item"
-    data-user-id="${id}"
-  >
-    <td>
-      <label 
-        class="adminPanel__showUserName"
-      >
-        ${data.get('userName')}
-      </label>
-      <input
-        type="text"
-        class="adminPanel__changeUserName hidden"
-        value="${data.get('userName')}"
-      >
-    </td>
-    <td>
-      <label 
-        class="adminPanel__showUserDepartment"
-      >
-        ${data.get('userDepartment')}
-      </label>
-      <select
-        name="userDepartment"
-        class="adminPanel__changeUserDepartment hidden"
-        id="userDepartment"
-      >
-        <option 
-          value="development"
-        >
-          development
-        </option>
-        <option 
-          value="bookkeeping"
-        >
-          bookkeeping
-        </option>
-        <option 
-          value="management"
-        >
-          management
-        </option>
-      </select>
-    </td>
-    <td>
-      <label 
-        class="adminPanel__dateOfCreation"
-      >
-        ${dateBuilder(new Date())}
-      </label>
-    </td>
-    <td>
-      <label 
-        class="adminPanel__dateOfChange"
-      >
-        ${dateBuilder(new Date())}
-      </label>
-    </td>
-    <td>
-      <button
-        type="button"
-        class="update"
-      >
-        Update
-      </button>
-      <button
-        type="button"
-        class="save hidden"
-      >
-        Save
-      </button>
-    </td>
-    <td>
-      <button
-        type="button"
-        class="destroy"
-      >
-        delete
-      </button>
-    </td>
-  </tr>
-  `)
+    userUpdateId = null;
+  } else {
+    const userForLocalStorage = {
+      id: id,
+      userName: data.get('userName'),
+      userDepartment: data.get('userDepartment'),
+      DateOfCreation: dateAdd,
+      DateOfChange: dateAdd,
+    }
+
+    localStorageUsers.push(userForLocalStorage);
+  };  
+
+  setLocalStorageUsers(localStorageUsers);
+
+  initUsers(localStorageUsers);
 });
 
 table.addEventListener('click', (event) => {
-  if ((!event.target.matches('.destroy'))
-    && (!event.target.matches('.update'))
-    && (!event.target.matches('.save'))) {
+  if ((!event.target.matches('#deleteItem'))
+    && (!event.target.matches('#updateItem'))) {
     return;
   }
   const item = event.target.closest('.adminPanel__table-item');
-  const userForQuestion = currentUsers.find(user => user.id === +item.dataset.userId);
-  const changeUserName = item.querySelector('.adminPanel__changeUserName');
-  const showUserName = item.querySelector('.adminPanel__showUserName');
-  const updateButton = item.querySelector('.update');
-  const saveButton = item.querySelector('.save');
-  const dateOfChangeField = item.querySelector('.adminPanel__dateOfChange');
-  const dateOfCreationField = item.querySelector('.adminPanel__dateOfCreation');
-  const showUserDepartment = item.querySelector('.adminPanel__showUserDepartment');
-  const changeUserDepartment = item.querySelector('.adminPanel__changeUserDepartment')
+  const userForQuestion = localStorageUsers.find(user => user.id === +item.dataset.userId);
 
-  if ((event.target.matches('.destroy'))) {
+  if ((event.target.matches('#deleteItem'))) {
     const result = confirm(`are you sure you want to delete user ${userForQuestion.userName}?`);
 
     if (!result) {
@@ -266,35 +188,14 @@ table.addEventListener('click', (event) => {
 
     item.remove();
 
-    currentUsers = currentUsers.filter(user => user.id !== +item.dataset.userId);
+    localStorageUsers = localStorageUsers.filter(user => user.id !== +item.dataset.userId);
 
-    localStorage.removeItem(`${userForQuestion.id}`);
+    setLocalStorageUsers(localStorageUsers);
   }
 
-  if ((event.target.matches('.update'))) {
-    changeUserName.classList.toggle('hidden');
-    showUserName.classList.toggle('hidden');
-    updateButton.classList.toggle('hidden');
-    saveButton.classList.toggle('hidden');
-    showUserDepartment.classList.toggle('hidden');
-    changeUserDepartment.classList.toggle('hidden');
+  if ((event.target.matches('#updateItem'))) {
+    userUpdateId = userForQuestion.id;
+    form.elements.userName.value = userForQuestion.userName;
+    form.elements.userDepartment.value = userForQuestion.userDepartment;
   }
-
-  if ((event.target.matches('.save'))) {
-    changeUserName.classList.toggle('hidden');
-    showUserName.classList.toggle('hidden');
-    updateButton.classList.toggle('hidden');
-    saveButton.classList.toggle('hidden');
-    showUserDepartment.classList.toggle('hidden');
-    changeUserDepartment.classList.toggle('hidden');
-
-    showUserName.textContent = changeUserName.value;
-    showUserDepartment.textContent = changeUserDepartment.value;
-    dateOfChangeField.textContent = dateBuilder(new Date());
-
-    localStorage.setItem(`${userForQuestion.id}`,
-      `${changeUserName.value}#${changeUserDepartment.value}#${dateOfCreationField.textContent}#${dateBuilder(new Date())}`
-    )
-  }
-  
 });
